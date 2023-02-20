6f6cfbc1983c4ab6c6e582b73309e7ef9a92cd57
Iskandar <eiscocyber@gmail.com>
2023-02-20 12:54:15 +0500

    updated at 12:54 20.02.2023

--- vazifa.py
+++ vazifa.py
@@ -1,6 +1,7 @@
-a = input().split() #how to input: 01.02.2023 12:30
-date = list(map(int, a[0].split('.')))
-time = list(map(int, a[1].split(':')))
-dic = {1:'yanvar',2:'fevral',3:'mart',4:'aprel',5:'may',6:'iyun',7:'iyul',8:'avgust',9:'sentyabr',10:'oktyabr',11:'noyabr',12:'dekabr'}
-print(f"\n{str(date[0]).zfill(2)}-{dic[date[1]]} 20{str(date[2])[2::]}-yil")
-print(f"{time[0]}-soat {time[1]}-minut")
+# a = input().split() #how to input: 01.02.2023 12:30
+# date = list(map(int, a[0].split('.')))
+# time = list(map(int, a[1].split(':')))
+# dic = {1:'yanvar',2:'fevral',3:'mart',4:'aprel',5:'may',6:'iyun',7:'iyul',8:'avgust',9:'sentyabr',10:'oktyabr',11:'noyabr',12:'dekabr'}
+# print(f"\n{str(date[0]).zfill(2)}-{dic[date[1]]} 20{str(date[2])[2::]}-yil")
+# print(f"{time[0]}-soat {time[1]}-minut"
+
